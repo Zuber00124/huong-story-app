77c4748d72594f14e4b23e5bfbb99dc3efa4fa4d
Zuber00124 <zuber00124@gmail.com>
2019-05-10 16:03:01 +0530

    client setup home and navbar

---
+++ client/src/components/Home.js
@@ -0,0 +1,4 @@
+import React from 'react'
+
+const Home = () => <div>Thisis HomePgae</div>
+export default Home
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,4 @@
+import React from 'react'
+
+const Navbar = () => <div>This is Nav</div>
+export default Navbar
